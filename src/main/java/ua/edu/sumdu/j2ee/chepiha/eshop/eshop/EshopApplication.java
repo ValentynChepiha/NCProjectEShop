4f41e62c20d43f2ac00fc825a418a58a05e5a08e
Valentyn Chepiha <valentin.chepiga@gmail.com>
2022-02-05 20:07:45 +0200

    Init project

---
+++ src/main/java/ua/edu/sumdu/j2ee/chepiha/eshop/eshop/EshopApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.j2ee.chepiha.eshop.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopApplication.class, args);
+	}
+
+}
