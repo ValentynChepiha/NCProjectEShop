9f74319611fc7c55f546bc0281eda851a406e4cc
Valentyn Chepiha <valentin.chepiga@gmail.com>
2022-02-13 23:40:07 +0200

    Fixed PreparedStatement, added diferent types

--- src/main/java/ua/edu/sumdu/j2ee/chepiha/eshop/eshop/models/services/CreationStatementOracle.java
+++ src/main/java/ua/edu/sumdu/j2ee/chepiha/eshop/eshop/models/services/CreationStatementOracle.java
@@ -5,25 +5,33 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CreationStatementOracle implements PreparedStatementCreator {
 
     private String sql;
-    private List<String> listStatements;
 
+    private List<String> listTypes;
+    private List<Object> listObjects;
 
     public CreationStatementOracle() {
-        listStatements = new ArrayList<>();
+        listTypes = new ArrayList<>();
+        listObjects = new ArrayList<>();
     }
 
     public void addStatement(String statement) {
-        listStatements.add(statement);
+        listTypes.add("string");
+        listObjects.add(statement);
+    }
+
+    public void addStatement(long statement) {
+        listTypes.add("long");
+        listObjects.add(statement);
     }
 
     public void clearStatements(){
-        listStatements.clear();
+        listTypes.clear();
+        listObjects.clear();
     }
 
     public void setSql(String sql) {
@@ -34,8 +42,15 @@ public class CreationStatementOracle implements PreparedStatementCreator {
     public PreparedStatement createPreparedStatement(
             final Connection con) throws SQLException {
         PreparedStatement ps = con.prepareStatement(sql, new String[] {"id"});
-        for(int i=0; i<listStatements.size(); i++){
-            ps.setString(i+1, listStatements.get(i));
+        for(int i=0; i<listObjects.size(); i++){
+            switch (listTypes.get(i)){
+                case "string":
+                    ps.setString(i+1, (String) listObjects.get(i));
+                    break;
+                case "long":
+                    ps.setLong(i+1, (Long) listObjects.get(i));
+                    break;
+            }
         }
         return ps;
     }
