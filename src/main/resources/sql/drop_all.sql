d9319a40ddcb88a0e729fab26d246fd7fa2235c2
Valentyn Chepiha <valentin.chepiga@gmail.com>
2022-02-24 00:08:17 +0200

    Added Flyway. Added migration DB

---
+++ src/main/resources/sql/drop_all.sql
@@ -0,0 +1,38 @@
+drop trigger "BI_LAB3_CHEPIHAVV_USER";
+drop sequence "LAB3_CHEPIHAVV_USER_SEQ";
+drop table LAB3_CHEPIHAVV_USER  cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_USER_ROLE";
+drop sequence "LAB3_CHEPIHAVV_USER_ROLE_SEQ";
+drop table "LAB3_CHEPIHAVV_USER_ROLE";
+
+
+drop trigger "BI_LAB3_CHEPIHAVV_ORDER_PRODUCTS";
+drop sequence "LAB3_CHEPIHAVV_ORDER_PRODUCTS_SEQ";
+drop table "LAB3_CHEPIHAVV_ORDER_PRODUCTS"   cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_ORDER";
+drop sequence "LAB3_CHEPIHAVV_ORDER_SEQ";
+drop table "LAB3_CHEPIHAVV_ORDER"   cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_PRODUCT";
+drop sequence "LAB3_CHEPIHAVV_PRODUCT_SEQ";
+drop table LAB3_CHEPIHAVV_PRODUCT  cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_STORAGE";
+drop sequence "LAB3_CHEPIHAVV_STORAGE_SEQ";
+drop table LAB3_CHEPIHAVV_STORAGE  cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_CLIENT";
+drop sequence "LAB3_CHEPIHAVV_CLIENT_SEQ";
+drop table LAB3_CHEPIHAVV_CLIENT  cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_LOCATION";
+drop sequence "LAB3_CHEPIHAVV_LOCATION_SEQ";
+drop table LAB3_CHEPIHAVV_LOCATION  cascade constraints;
+
+drop trigger "BI_LAB3_CHEPIHAVV_BRAND";
+drop sequence "LAB3_CHEPIHAVV_BRAND_SEQ";
+drop table "LAB3_CHEPIHAVV_BRAND"  cascade constraints;
+/
+
